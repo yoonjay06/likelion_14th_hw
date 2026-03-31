4398c5a4e4969203cbb75c7e729947462524dfec
yoonjay06 <yjaewon1020@gmail.com>
2026-03-31 13:52:36 +0900

    리드미생성 장고 기본세팅

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7w$%ka)p&h=i1=ibg)#@#@&9nf2f^!%#xdu-m&vjeqa8@8!=b!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'ASIA/Seoul'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
