f89334ebab6a5da3bd91949798afb1aa3527bb35
yoonjay06 <yjaewon1020@gmail.com>
2026-04-07 12:33:39 +0900

    https://github.com/yoonjay06/14th_git_hw.git

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    context = {
+        'generation': 14,                   
+        'info': {                           
+            'html': 'templates폴더 안에 메인페이지 생성',
+            'views': 'views.py 작성 후 메인페이지 렌더링',
+            'secondpg': 'secondpage도 동일하게 진행'
+        },
+        'shortcuts': [
+            'HTML 링크 연결 : secondpage를 누르면 해당페이지로 넘어가게 설정',
+            '변수 사용 : 변수 -> {{변수}}, 변수의 속성 -> {{변수.속성}}, list의 각 index -> {{list.값의 위치}}, dict의 value값 -> {{dict.key}} ',
+            '반복문 : for문 사용 예시 -> {% for product in products %} <p>{{product.name}}</p> {% endfor %}',
+            '조건문 : if문 사용 예시 -> {% if 조건 %} 참이네요! {% else %} 거짓이네요! {% endif %} ',
+            'Template 필터 : 필터는 주로 변수와 함께 쓰이며 원하는 형태로 값을 변환하기 위해 사용한다.',               
+        ]
+    }
+    return render(request, 'main/mainpage.html', context)
+def secondpage(request):
+    return render(request, 'main/secondpage.html')
--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'project', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +116,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
